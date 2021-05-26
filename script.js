f02f4e86da4764bbd1d7298ee802bacda668413e
Luã Streit <lua.streit@prontomail.com>
2021-05-26 17:36:58 +0200

    Flow explorer and opening explorer in good shape

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function clamp(x, m, M) {
 
 class Chessboard {
 
-  constructor(size, blackColor, whiteColor) {
+  constructor(div, size, blackColor, whiteColor) {
     this.initial_state = {
       'wr-L': 'a1', 'wn-L': 'b1', 'wb-L': 'c1', 'wq':   'd1', 'wk':   'e1', 'wb-R': 'f1', 'wn-R': 'g1', 'wr-R': 'h1',
       'wp-a': 'a2', 'wp-b': 'b2', 'wp-c': 'c2', 'wp-d': 'd2', 'wp-e': 'e2', 'wp-f': 'f2', 'wp-g': 'g2', 'wp-h': 'h2',
@@ -34,7 +34,7 @@ class Chessboard {
     this.scale = d3.scaleLinear().domain([0, 8]).range([0, size]);
     this.scale.clamp(true);
 
-    this.svg = d3.select("#chess-container").append("svg")
+    this.svg = d3.select(div).append("svg")
           .attr("width", size)
           .attr("height", size)
           .attr("display", "block")
@@ -81,17 +81,18 @@ class Chessboard {
         .text(String(n));
     });
 
+    this.flowGroup = this.svg
+        .append('g')
+        .attr('id', 'flows');
+
     this.piecesGroup = this.svg.append("g").attr("id", "pieces");
     this.drawPieces();
   }
 
   showOpening(moves) {
-    if (this.ongoing || moves.length == 0) {
+    if (this.openingInterval || moves.length == 0) {
       return;
     }
-    this.ongoing = true;
-
-
 
     let i = 0;
     if (JSON.stringify(this.state) == JSON.stringify(this.initial_state)) {
@@ -102,14 +103,13 @@ class Chessboard {
     else {
       this.reset();
     }
-    let interval = d3.interval(() => {
+    this.openingInterval = d3.interval(() => {
       if (i < moves.length) {
         this.movePiece(moves[i][0], moves[i][1]);
         this.drawPieces();
         i += 1;
       } else {
-        interval.stop();
-        this.ongoing = false;
+        this.openingInterval.stop();
         return;
       }
     }, 750);
@@ -118,6 +118,12 @@ class Chessboard {
   centerPiece(pos) {
     let x = this.scale(chess_to_xy(pos)[0]) + 0.15 * this.tileSize;
     let y = this.scale(chess_to_xy(pos)[1]) + 0.15 * this.tileSize;
+    return [x, y]
+  }
+
+  centerPosition(pos) {
+    let x = this.scale(chess_to_xy(pos)[0]) + 0.5 * this.tileSize;
+    let y = this.scale(chess_to_xy(pos)[1]) + 0.5 * this.tileSize;
     return [x, y]
   }
 
@@ -137,17 +143,18 @@ class Chessboard {
         .attr("x", (piece) => this.centerPiece(this.state[piece])[0])
         .attr("y", (piece) => this.centerPiece(this.state[piece])[1]);
 
-    let e = pieces.enter()
+    this.enter = pieces.enter()
       .append("image")
         .attr("class", "piece")
         .attr("id", (piece) => piece)
         .attr("href", (piece) => this.mapping(piece))
         .attr("height", 0)
         .attr("width", 0)
+        .attr("z-index", 1)
         .attr("x", (piece) => this.centerPiece(this.state[piece])[0] + 0.35 * this.tileSize)
         .attr("y", (piece) => this.centerPiece(this.state[piece])[1] + 0.35 * this.tileSize);
 
-    e.transition()
+    this.enter.transition()
       .duration(250)
       .ease(d3.easeLinear)
         .attr("height", 0.7 * this.tileSize)
@@ -155,9 +162,8 @@ class Chessboard {
         .attr("x", (piece) => this.centerPiece(this.state[piece])[0])
         .attr("y", (piece) => this.centerPiece(this.state[piece])[1]);
 
-    e.on("mouseover", function(d){d3.select(this).style("cursor", "pointer")})
-        .on("mouseout",  function(d){d3.select(this).style("cursor", null)})
-        .on("click", (d) => this.tutorial(d));
+    this.enter.on("mouseover", function(d){d3.select(this).style("cursor", "pointer")})
+        .on("mouseout",  function(d){d3.select(this).style("cursor", null)});
 
 
     let self_ = this;
@@ -182,13 +188,12 @@ class Chessboard {
 
       d3.select(this)
         .attr("x", x)
-        .attr("y", y)
-        .style("z-index", null);
+        .attr("y", y);
 
       self_.movePiece(piece, position);
     }
 
-    e.call(
+    this.enter.call(
       d3.drag()
         .on("drag", dragged)
         .on("end", dragended)
@@ -248,6 +253,87 @@ class Chessboard {
       this.drawPieces();
     }
   }
+
+  showFlow(piece, flows, progressbar) {
+    // Removing anything created previously
+    if (this.flowInterval) {
+      this.flowInterval.stop();
+    }
+    this.svg.selectAll('.flow').remove();
+
+    // Hiding all pieces except the selected one
+    this.svg.selectAll('.piece').attr('opacity', 0.15);
+    this.svg.select('#' + piece).attr('opacity', 1);
+
+    let line = d3.line()
+        .x(d => d.x)
+        .y(d => d.y)
+        .curve(d3.curveCardinal.tension(0.4));
+
+    // Taking a set of sub-samples (for performance)
+    flows = _.sampleSize(flows, Math.min(1000, flows.length));
+
+    // A function to create random jitter to the x-y positions of the flows
+    let jitter = () => this.tileSize / 6 * (Math.random() - 0.5)
+
+    let T = d3.max(flows, d => d3.max(d.positions.map(([t, l]) => t)));
+    progressbar.text('0 / ' + String(T))
+    let paths = {};
+    let t = 0;
+    let [rx, ry] = [0, 0]
+    this.flowInterval = d3.interval(() => {
+      if (t <= T) {
+        flows.forEach((f, i) => {
+          if (f.positions.length > 0) {
+            if (f.positions[0][0] == t) {
+              [rx, ry] = [jitter(), jitter()]
+              paths[i] = {
+                random: [rx, ry],
+                data: f.positions[0][1].map(d => {
+                  let [x, y] = this.centerPosition(d);
+                  return {x: x + jitter(), y: y + jitter()}
+                })
+              }
+            }
+            else {
+              f.positions.slice(1).forEach(([tp, l], j) => {
+                if (tp == t) {
+                  if (l.length == 1 || j == f.positions.length - 2) {
+                    delete paths[i]
+                  }
+                  else {
+                    let [x, y] = this.centerPosition(l[1]);
+                    [rx, ry] = paths[i].random;
+                    paths[i].data = paths[i].data.concat({x: x + jitter(), y: y + jitter()})
+                  }
+                }
+              })
+            }
+            if (i in paths) {
+              if ('svg' in paths[i]) {
+                paths[i].svg.remove()
+              }
+              paths[i].svg = this.flowGroup
+                .append('path')
+                  .attr('class', 'flow')
+                  .attr('stroke', 'cyan')
+                  .attr('stroke-width', 2 * 1000 / flows.length)
+                  .attr('opacity', 0.2)
+                  .attr('fill', 'none')
+                  .attr('z-index', 2)
+                  .attr('d', line(paths[i].data));
+            }
+          }
+        })
+        progressbar.text(String(t) + ' / ' + String(T))
+        t ++;
+      }
+      else {
+        this.flowInterval.stop();
+        return;
+      }
+    }, 100);
+  }
 }
 
 
@@ -262,8 +348,9 @@ function whenDocumentLoaded(action) {
 
 whenDocumentLoaded(() => {
   let size = 800;
-  let board = new Chessboard(size, "#545454", "#AAAAAA");
 
+  // OPENINGS
+  let openingBoard = new Chessboard('#opening-chess-container', size, "#AA5454", "#EEAAAA");
 
   d3.json("data/openings.json", function (error, data) {
     let selector = d3.select("#opening-selector")
@@ -282,23 +369,27 @@ whenDocumentLoaded(() => {
     selector.on("change", () => {
       curr = 0;
       states = data[selector.property("value")].states
-      board.reset()
+      if (openingInterval) {
+        openingInterval.stop();
+      }
+      openingBoard.reset()
     })
 
+    let openingInterval = undefined;
 
     function back() {
-      if (!board.ongoing && curr > 0) {
+      if (openingInterval != null && curr > 0) {
         curr -= 1;
-        board.state = JSON.parse(JSON.stringify(states[curr]));
-        board.drawPieces();
+        openingBoard.state = JSON.parse(JSON.stringify(states[curr]));
+        openingBoard.drawPieces();
       }
     }
 
     function next(skip=false) {
-      if (!board.ongoing && curr < states.length - 1) {
+      if (openingInterval != null && curr < states.length - 1) {
         curr += 1;
-        board.state = JSON.parse(JSON.stringify(states[curr]));
-        board.drawPieces();
+        openingBoard.state = JSON.parse(JSON.stringify(states[curr]));
+        openingBoard.drawPieces();
       }
     }
 
@@ -315,19 +406,20 @@ whenDocumentLoaded(() => {
       .attr("id", "button-play")
       .text("play")
       .on("click", () => {
+        if (openingInterval != null) {return;}
         if (curr < states.length - 1) {
           next();
         }
         else {
-          board.reset();
+          openingBoard.reset();
           curr = 0;
         }
-        let interval = d3.interval(() => {
+        openingInterval = d3.interval(() => {
           if (curr < states.length - 1) {
             next()
           } else {
-            interval.stop();
-            this.ongoing = false;
+            openingInterval.stop();
+            openingInterval = undefined;
             return;
           }
         }, 750);
@@ -343,4 +435,51 @@ whenDocumentLoaded(() => {
       .on("mouseout",  function(d){d3.select(this).style("cursor", null)})
 
   })
+
+  // FLOWS
+  let progressbar = d3.select('#flow-info')
+  let elobar = d3.select('#flow-controller')
+      .append('svg')
+      .attr('width', 0.9 * size + 2)
+      .attr('height', 40)
+
+  let minELO = 816;
+  let maxELO = 2475;
+
+  let selectedElo = [minELO, maxELO];
+
+  let eloscale = d3.scaleLinear()
+      .domain([minELO, maxELO])
+      .range([0, 0.9 * size])
+
+  let brush = d3.brushX()
+      .extent([[0, 0], [0.9 * size, 35]])
+      .on('brush', function() {
+        let [x1, x2] = d3.event.selection;
+        selectedElo = [eloscale.invert(x1), eloscale.invert(x2)]
+      })
+
+  let eloAxis = d3.axisBottom()
+      .scale(eloscale)
+
+  elobar
+      .append('g')
+      .attr('id', 'eloAxis')
+      .attr('class', 'axisWhite')
+      .call(eloAxis)
+
+  elobar.append('g')
+      .attr('id', 'eloBrush')
+      .call(brush)
+      .call(brush.move, [0, 0.9*size])
+
+
+  let flowBoard = new Chessboard('#flow-chess-container', size, "#545454", "#AAAAAA");
+  flowBoard.enter.on("click", d => {
+    d3.json("data/flows/" + d + '.json', function (error, data) {
+      let filtered = data.flatMap(game => (selectedElo[0] <= game.ELO && game.ELO <= selectedElo[1]) ? [game] : [])
+      flowBoard.showFlow(d, filtered, progressbar)
+    })
+  })
+
 });
